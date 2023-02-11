7651f048c6fc450585b96b7ef33894d9a1af6200
wyang14 <weili.yang@yahooinc.com>
2023-02-11 01:15:13 +0000

    feat: shared UI components

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -3,9 +3,16 @@
 </template>
 
 <script>
+import { Head } from '@inertiajs/inertia-vue3'
+import Layout from '@/Shared/Layout.vue'
+import { Inertia } from '@inertiajs/inertia'
 export default {
+    components: {
+        Head,
+    },
     props: {
         eth_address: String
-    }
+    },
+    layout: Layout,
 }
 </script>
