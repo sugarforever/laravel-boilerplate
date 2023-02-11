c2e469e46c6511c912125b662f955efe7c7bcb59
wyang14 <weili.yang@yahooinc.com>
2023-02-11 01:09:04 +0000

    added untracked files

---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>{{ eth_address }}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        eth_address: String
+    }
+}
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <script type="text/javascript" async src="https://platform.twitter.com/widgets.js"></script>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    @inertiaHead
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
