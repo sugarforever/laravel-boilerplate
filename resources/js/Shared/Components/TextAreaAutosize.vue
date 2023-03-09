4a46135fe04e6ea2611dd98a33a7712a9d92cf1f
wyang14 <weili.yang@yahooinc.com>
2023-03-09 18:30:34 +0000

    generalize the TextAreaAutosize component

--- resources/js/Shared/Components/TextAreaAutosize.vue
+++ resources/js/Shared/Components/TextAreaAutosize.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onBeforeUnmount, onMounted, onUnmounted, ref } from "vue";
+import { onBeforeUnmount, onMounted, ref } from "vue";
 import autosize from "autosize/dist/autosize";
 
 const props = defineProps({
@@ -10,11 +10,9 @@ const emit = defineEmits(['update:modelValue']);
 const el = ref();
 
 onMounted(() => autosize(el.value));
-
 onBeforeUnmount(() => autosize.destroy(el.value));
 </script>
+
 <template>
-  <textarea ref="el"
-    class="block w-full text-sm rounded-md border-gray-300 shadow-sm focus:border-blue-400 focus:ring-blue-400"
-    @input="emit('update:modelValue', $event.target.value)">{{ props.modelValue }}</textarea>
+  <textarea ref="el" @input="emit('update:modelValue', $event.target.value)" v-model="props.modelValue"></textarea>
 </template>
