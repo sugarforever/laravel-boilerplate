19f2185eaf7312d96cb55e4b1cca2625a57da9cb
wyang14 <weili.yang@yahooinc.com>
2023-03-09 20:40:25 +0000

    handle the case of resetting the input

--- resources/js/Shared/Components/TextAreaAutosize.vue
+++ resources/js/Shared/Components/TextAreaAutosize.vue
@@ -9,6 +9,11 @@ const emit = defineEmits(['update:modelValue']);
 
 const el = ref();
 
+watch(() => props.modelValue, (newValue, oldValue) => {
+  if (oldValue != "" && newValue == "") {
+    el.value.style.height = 'auto';
+  }
+});
 onMounted(() => autosize(el.value));
 
 onBeforeUnmount(() => autosize.destroy(el.value));
@@ -17,5 +22,5 @@ onBeforeUnmount(() => autosize.destroy(el.value));
 <template>
   <textarea ref="el"
     class="block w-full text-sm rounded-md border-gray-300 shadow-sm focus:border-blue-400 focus:ring-blue-400"
-    @input="emit('update:modelValue', $event.target.value)">{{ props.modelValue }}</textarea>
+    @input="emit('update:modelValue', $event.target.value)" v-model="props.modelValue"></textarea>
 </template>
