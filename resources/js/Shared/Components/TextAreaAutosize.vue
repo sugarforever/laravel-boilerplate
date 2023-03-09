7ef0006142ea7c76fcea43401a3399cccb6d0c8a
wyang14 <weili.yang@yahooinc.com>
2023-03-09 18:00:53 +0000

    autoresizable textarea

---
+++ resources/js/Shared/Components/TextAreaAutosize.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onBeforeUnmount, onMounted, onUnmounted, ref } from "vue";
+import autosize from "autosize/dist/autosize";
+
+const props = defineProps({
+  modelValue: String
+});
+const emit = defineEmits(['update:modelValue']);
+
+const el = ref();
+
+onMounted(() => autosize(el.value));
+
+onBeforeUnmount(() => autosize.destroy(el.value));
+</script>
+<template>
+  <textarea ref="el"
+    class="block w-full text-sm rounded-md border-gray-300 shadow-sm focus:border-blue-400 focus:ring-blue-400"
+    @input="emit('update:modelValue', $event.target.value)">{{ props.modelValue }}</textarea>
+</template>
