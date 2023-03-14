54bbdd9f3c4a5cf4db03c554f37208e46be22929
wyang14 <weili.yang@yahooinc.com>
2023-03-14 17:44:06 +0000

    feat: added fontawesome support

--- resources/js/app.js
+++ resources/js/app.js
@@ -48,10 +48,18 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
 createInertiaApp({
   resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
+      .component('font-awesome-icon', FontAwesomeIcon)
       .use(plugin)
       .mount(el)
   },
