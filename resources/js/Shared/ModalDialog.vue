8f31ff4fe2bf01d5392e31fb3b97f5617baa101f
wyang14 <william.1022@gmail.com>
2023-07-17 11:55:21 +0100

    modal dialog

---
+++ resources/js/Shared/ModalDialog.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue"
+
+const props = defineProps({
+  display: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const displayData = ref(props.display)
+
+function hideDialog() {
+  displayData.value = false
+}
+</script>
+<template>
+  <div @click="hideDialog" :class="`absolute inset-0 bg-blue-100/75 flex items-center justify-center z-10 ${displayData ? '' : 'hidden'}`">
+    <div @click="(event) => event.stopPropagation()">
+      <slot />
+    </div>
+  </div>
+</template>
